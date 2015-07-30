e29eeaf94340ba7a04c897e78794c9cc4573d85b
CapXilinx <melik-merkumians@acin.tuwien.ac.at>
2015-07-30 10:56:00 +0200

    Removes explicit statement of provider in package, as suggested in chef documentation

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,10 +5,6 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 package 'libtool' do
-	case node['platform']
-	when 'ubuntu', 'debian'
-		provider Chef::Provider::Package::Apt
-	end
-action :install
+  action :install
 end
 
